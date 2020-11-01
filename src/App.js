53bda1b82d4b8c24a7a89f930352a5a829912475
Silvio <silvio.dias@etec.sp.gov.br>
2020-10-31 23:32:18 -0300

    adicionar  ListaDeSignos.js

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import ListaDeSignos from './components/ListaDeSignos'
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ListaDeSignos />
+    </div>
+  );
+}
+
+export default App;
