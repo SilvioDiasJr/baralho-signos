cdcc9a56b00852f5b608882efa4d6ca595c7c2c7
Silvio <silvio.dias@etec.sp.gov.br>
2020-10-31 23:31:36 -0300

    criar data de signos

---
+++ src/data/signos.js
@@ -0,0 +1,14 @@
+export default [
+  {id: 1, signo: 'Aquário', dataInicio: '21/01', dataFim: '19/02', imagem: 'assets/aquario.jpg'},
+  {id: 2, signo: 'Peixes', dataInicio: '20/02', dataFim: '20/03', imagem: 'assets/peixes.jpg'},
+  {id: 3, signo: 'Áries', dataInicio: '21/03', dataFim: '20/04', imagem: 'assets/aries.jpg'},
+  {id: 4, signo: 'Touro', dataInicio: '21/04', dataFim: '21/05', imagem: 'assets/touro.jpg'},
+  {id: 5, signo: 'Gêmeos', dataInicio: '22/05', dataFim: '21/06', imagem: 'assets/gemeos.jpg'},
+  {id: 6, signo: 'Câncer', dataInicio: '21/06', dataFim: '23/07', imagem: 'assets/cancer.jpg'},
+  {id: 7, signo: 'Leão', dataInicio: '24/07', dataFim: '23/08', imagem: 'assets/leao.jpg'},
+  {id: 8, signo: 'Virgem', dataInicio: '24/08', dataFim: '23/09', imagem: 'assets/virgem.jpg'},
+  {id: 9, signo: 'Libra', dataInicio: '24/09', dataFim: '23/10', imagem: 'assets/libra.jpg'},
+  {id: 10, signo: 'Escorpião', dataInicio: '24/10', dataFim: '22/11', imagem: 'assets/escorpiao.jpg'},
+  {id: 11, signo: 'Sagitário', dataInicio: '23/11', dataFim: '21/12', imagem: 'assets/sagitario.jpg'},
+  {id: 12, signo: 'Capricórnio', dataInicio: '22/12', dataFim: '20/01', imagem: 'assets/capricornio.jpg'}
+]
